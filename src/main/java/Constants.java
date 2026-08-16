9172d26413f51c1b707fdcb1240b7b02889244c3
Sakthi-PC <mssakthieesvar@gmail.com>
2026-08-16 21:37:15 +0800

    Added greeting

---
+++ src/main/java/Constants.java
@@ -0,0 +1,6 @@
+// This class will have constant items that I will use throughout the project
+public class Constants {
+    public static void divider() {
+        System.out.println("===========================================================");
+    }
+}
---
+++ src/main/java/Rambo.java
@@ -0,0 +1,24 @@
+
+public class Rambo {
+  private static void greeting() {
+    String banner = " (                           \n"
+        + ")\\ )                 )\n"
+        + "(()/(   )    )    ( /(\n"
+        + " /(_)| /(   (     )\\()) (\n"
+        + "(_)) )(_))  )\\  '((_)\\  )\\\n"
+        + "| _ ((_)_ _((_)) | |(_)((_)\n"
+        + "|   / _` | '  \\()| '_ Y _ \\\n"
+        + "|_|_\\__,_|_|_|_| |_.__|___/\n"
+        + "                            ";
+
+    Constants.divider();
+    System.out.println(banner);
+    System.out.println("Hello! I am Rambo.\nWhat can I do for you?");
+    Constants.divider();
+  }
+
+  public static void main(String[] args) {
+    // When I first start this program, I would like to greet first
+    greeting();
+  }
+}
